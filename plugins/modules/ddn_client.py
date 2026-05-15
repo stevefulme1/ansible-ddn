bb1ac497d61aa7e556b93ac0c01b244d212cb2c2
sfulmer <sfulmer@redhat.com>
2026-05-15 14:47:44 -0400

    Expand to 50 modules for full platform coverage

---
+++ plugins/modules/ddn_client.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2026 Steve Fulmer
+# Apache-2.0 (see LICENSE)
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+"""ddn_client module."""
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: ddn_client
+short_description: Manage DDN Lustre clients
+description:
+    - Manage DDN Lustre clients.
+version_added: "1.0.0"
+author:
+    - Steve Fulmer (@stevefulme1)
+options:
+    state:
+        description: Desired state of the resource.
+        type: str
+        default: present
+        choices: [present, absent]
+    host:
+        description: API host address.
+        type: str
+        required: true
+    client_id:
+        description: Unique identifier.
+        type: str
+    name:
+        description: Display name.
+        type: str
+    username:
+        description: Authentication username.
+        type: str
+    password:
+        description: Authentication password.
+        type: str
+    api_key:
+        description: API key for authentication.
+        type: str
+    validate_certs:
+        description: Validate SSL certificates.
+        type: bool
+        default: true
+"""
+
+EXAMPLES = r"""
+- name: Create
+  stevefulme1.ddn.ddn_client:
+    host: api.example.com
+    name: my-resource
+    state: present
+
+- name: Delete
+  stevefulme1.ddn.ddn_client:
+    host: api.example.com
+    client_id: "example-id"
+    state: absent
+"""
+
+RETURN = r"""
+client:
+    description: Resource details.
+    returned: on success
+    type: dict
+"""
+
+from ansible.module_utils.basic import AnsibleModule
+
+try:
+    from ansible_collections.stevefulme1.ddn.plugins.module_utils.api_client import ApiClient
+    HAS_CLIENT = True
+except ImportError:
+    HAS_CLIENT = False
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            state=dict(type="str", default="present", choices=["present", "absent"]),
+            client_id=dict(type="str"),
+            name=dict(type="str"),
+            host=dict(type="str", required=True),
+            username=dict(type="str"),
+            password=dict(type="str", no_log=True),
+            api_key=dict(type="str", no_log=True),
+            validate_certs=dict(type="bool", default=True),
+        ),
+        supports_check_mode=True,
+        required_if=[("state", "absent", ("client_id",))],
+    )
+
+    if not HAS_CLIENT:
+        module.fail_json(msg="Required Python libraries not found.")
+
+    client = ApiClient(module)
+    state = module.params["state"]
+    resource_id = module.params.get("client_id")
+
+    if state == "present":
+        if resource_id:
+            result = client.update("client", resource_id, module.params)
+        else:
+            if module.check_mode:
+                module.exit_json(changed=True)
+            result = client.create("client", module.params)
+        module.exit_json(changed=True, client=result)
+    else:
+        if module.check_mode:
+            module.exit_json(changed=True)
+        client.delete("client", resource_id)
+        module.exit_json(changed=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ plugins/modules/ddn_mdt.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2026 Steve Fulmer
+# Apache-2.0 (see LICENSE)
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+"""ddn_mdt module."""
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: ddn_mdt
+short_description: Manage DDN Metadata Targets
+description:
+    - Manage DDN Metadata Targets.
+version_added: "1.0.0"
+author:
+    - Steve Fulmer (@stevefulme1)
+options:
+    state:
+        description: Desired state of the resource.
+        type: str
+        default: present
+        choices: [present, absent]
+    host:
+        description: API host address.
+        type: str
+        required: true
+    mdt_id:
+        description: Unique identifier.
+        type: str
+    name:
+        description: Display name.
+        type: str
+    username:
+        description: Authentication username.
+        type: str
+    password:
+        description: Authentication password.
+        type: str
+    api_key:
+        description: API key for authentication.
+        type: str
+    validate_certs:
+        description: Validate SSL certificates.
+        type: bool
+        default: true
+"""
+
+EXAMPLES = r"""
+- name: Create
+  stevefulme1.ddn.ddn_mdt:
+    host: api.example.com
+    name: my-resource
+    state: present
+
+- name: Delete
+  stevefulme1.ddn.ddn_mdt:
+    host: api.example.com
+    mdt_id: "example-id"
+    state: absent
+"""
+
+RETURN = r"""
+mdt:
+    description: Resource details.
+    returned: on success
+    type: dict
+"""
+
+from ansible.module_utils.basic import AnsibleModule
+
+try:
+    from ansible_collections.stevefulme1.ddn.plugins.module_utils.api_client import ApiClient
+    HAS_CLIENT = True
+except ImportError:
+    HAS_CLIENT = False
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            state=dict(type="str", default="present", choices=["present", "absent"]),
+            mdt_id=dict(type="str"),
+            name=dict(type="str"),
+            host=dict(type="str", required=True),
+            username=dict(type="str"),
+            password=dict(type="str", no_log=True),
+            api_key=dict(type="str", no_log=True),
+            validate_certs=dict(type="bool", default=True),
+        ),
+        supports_check_mode=True,
+        required_if=[("state", "absent", ("mdt_id",))],
+    )
+
+    if not HAS_CLIENT:
+        module.fail_json(msg="Required Python libraries not found.")
+
+    client = ApiClient(module)
+    state = module.params["state"]
+    resource_id = module.params.get("mdt_id")
+
+    if state == "present":
+        if resource_id:
+            result = client.update("mdt", resource_id, module.params)
+        else:
+            if module.check_mode:
+                module.exit_json(changed=True)
+            result = client.create("mdt", module.params)
+        module.exit_json(changed=True, mdt=result)
+    else:
+        if module.check_mode:
+            module.exit_json(changed=True)
+        client.delete("mdt", resource_id)
+        module.exit_json(changed=True)
+
+
+if __name__ == "__main__":
+    main()
